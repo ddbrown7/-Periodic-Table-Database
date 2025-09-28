1c575d425868d80c2dbb27f354b26e15febc2b04
ddbrown7 <ddbrown7@mail.fhsu.edu>
2025-09-28 19:51:32 -0400

    feat: add script entry point

---
+++ element.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only --no-align -c"
+
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+  exit 0
+fi
+
+# TODO: implement element lookup
