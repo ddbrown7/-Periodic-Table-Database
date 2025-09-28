d7832265ab57805cb055cf1b617f3e2ebfefffea
ddbrown7 <ddbrown7@mail.fhsu.edu>
2025-09-28 19:57:02 -0400

    fix: harden argument handling

--- element.sh
+++ element.sh
@@ -11,12 +11,13 @@ INPUT="$1"
 if [[ $INPUT =~ ^[0-9]+$ ]]; then
   CONDITION="e.atomic_number = $INPUT"
 else
-  CONDITION="e.symbol ILIKE '$INPUT' OR e.name ILIKE '$INPUT'"
+  SAFE_INPUT=$(sed "s/'/''/g" <<< "$INPUT")
+  CONDITION="e.symbol ILIKE '$SAFE_INPUT' OR e.name ILIKE '$SAFE_INPUT'"
 fi
 
-QUERY="SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE $CONDITION;"
+QUERY="SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE $CONDITION ORDER BY e.atomic_number LIMIT 1;"
 
-RESULT=$($PSQL "$QUERY")
+RESULT=$($PSQL "$QUERY" | sed '/^$/d')
 
 if [[ -z $RESULT ]]; then
   echo "I could not find that element in the database."
