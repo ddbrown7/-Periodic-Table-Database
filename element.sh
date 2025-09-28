6ebedbc6a757a666be159b5f1ca6e1812de0bc24
ddbrown7 <ddbrown7@mail.fhsu.edu>
2025-09-28 19:51:50 -0400

    feat: support numeric lookups

--- element.sh
+++ element.sh
@@ -7,4 +7,21 @@ if [[ -z $1 ]]; then
   exit 0
 fi
 
-# TODO: implement element lookup
+QUERY=""
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  QUERY="SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE e.atomic_number = $1;"
+else
+  echo "Element lookup for symbols or names is not implemented yet."
+  exit 0
+fi
+
+RESULT=$($PSQL "$QUERY")
+
+if [[ -z $RESULT ]]; then
+  echo "I could not find that element in the database."
+  exit 0
+fi
+
+IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<<"$RESULT"
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
